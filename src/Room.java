1b6c0258c3af63cc4ca7c8a3dbb66437e182cfbe
jonathansu <jonathansu@10.250.2.8>
2016-11-16 12:30:17 -0800

    test

--- src/Room.java
+++ src/Room.java
@@ -16,18 +16,21 @@ public class Room
 	 * Consturctor: creates a room object.
 	 * @param roomType String value stating whether the room is Luxury or Economic
 	 */
-	public Room(String roomType) 
+	public Room(String roomType,String roomNumber) 
 	{
 		//test = new
 		this.roomType = roomType.toLowerCase();
 		if (roomType.startsWith("l"))
 		{
 			roomType = "Luxurious";
+			cost = 200;
 		}
 		else if (roomType.startsWith("e"))
 		{
 			roomType = "Economic";
+			cost = 80;
 		}
+		this.roomNumber = roomNumber;
 		reservationsMade = new TreeMap<GregorianCalendar, Reservation>();
 	}
 	

1d95f7931df16d5a140b11bdabb9e99867ae7a61
JonCS <sogamaple@gmail.com>
2016-12-05 09:38:16 -0800

    Update HotelManager.java

--- src/HotelManager.java
+++ src/HotelManager.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;

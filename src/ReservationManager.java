01734440cb72efdec86eb8dc9dfa63458de42a32
JonCS <sogamaple@gmail.com>
2016-12-06 00:39:39 -0800

    Moved HotelManager to ReservationManager

--- src/ReservationManager.java
+++ src/ReservationManager.java
@@ -337,4 +337,89 @@ public class ReservationManager implements Serializable
 	{
 		currentAccount.clearTransaction();
 	}
+	public ArrayList<Room> findRoom(GregorianCalendar startDate, GregorianCalendar endDate, String type)
+	{
+		ArrayList<Room> openRooms = new ArrayList<>();
+		
+		outsideLoop:
+		for (Room r : rooms)
+		{
+			GregorianCalendar temp2 = startDate;
+			while(!temp2.equals(endDate))		
+			{
+				if (r.getAvailability().containsKey(temp2))
+				{
+					continue outsideLoop;
+				}
+				temp2.add(Calendar.DAY_OF_MONTH, 1);
+			}
+			openRooms.add(r);
+			
+		}
+		return openRooms;
+		
+	}
+
+	public Room getRoom(int roomNumber)
+	{
+		return rooms[roomNumber];
+	}
+
+	public void signIn() //prob not needed
+	{
+		
+	}
+
+	public void quit() //prob not needed, just save and close frames?
+	{
+		saveReservation();
+	}
+
+	public void saveReservation()
+	{
+		try{
+	        FileOutputStream fileOut = new FileOutputStream("events.ser");
+	        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+	        //out.writeObject(test);
+	        out.writeObject(this);
+	        out.close();
+	        fileOut.close();
+	    }catch(IOException i) {
+	    	i.printStackTrace();
+	    }
+	}
+
+	public void viewInformation()
+	{
+
+	}
+
+	public void loadReservation()
+	{
+		File file = new File("events.ser");
+		if(!file.exists())
+			return;
+		try {
+	         FileInputStream fileIn = new FileInputStream("events.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         //test = (ArrayList<Object>) in.readObject();
+	         this = (ReservationManager) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i) {
+	         i.printStackTrace();
+	         return;
+	      }catch(ClassNotFoundException c) {
+		         System.out.println("Classes not found");
+		         c.printStackTrace();
+		         return;
+		  }
+		//am = (AccountManager) test.get(0);
+		//rm = (ReservationManager) test.get(1);
+	}
+	
+	public void printEmptyRooms()
+	{
+		
+	}
 }
